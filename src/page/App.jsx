6a4f123a292951c8c5b9d30089da9c24e23d3f1a
Dirga <22523286@students.uii.ac.id>
2025-03-10 23:53:41 +0700

    Perubahan

---
+++ src/page/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+function App() {
+  
+  return (
+    <div className='bg-black h-screen flex flex-col items-center justify-center'>
+      <h1 className='text-4xl text-white font-sans'>Dirga's Website</h1>
+      <p className='text-white font-regular mt-2.5 text-xl'>This website is still under development</p>
+    </div>
+  )
+}
+
+export default App
