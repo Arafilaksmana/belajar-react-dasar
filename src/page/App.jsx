17a967b30fd02d731e552b081de1d2f09dfcb35c
Dirga <22523286@students.uii.ac.id>
2025-03-10 23:55:48 +0700

    perubahan

--- src/page/App.jsx
+++ src/page/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 function App() {
   
   return (
-    <div className='bg-black h-screen flex flex-col items-center justify-center'>
+    <div className='bg-black h-screen w-full flex flex-col items-center justify-center'>
       <h1 className='text-4xl sm:text-2xl text-white font-sans'>Dirga's Website</h1>
       <p className='text-white font-regular mt-2.5 text-xl'>This website is still under development</p>
     </div>
