8b9a5f449a697c0ce8ac3baf63b0f79c8fedc5df
Dirga <22523286@students.uii.ac.id>
2025-04-07 22:06:30 +0700

    changes

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -93,15 +93,15 @@ function Navbar() {
                     <img src="/icons/right-up.png" alt="" className="w-3 h-3" />
                 </div>
                 <div className="flex space-x-1">
-                    <a href="">X</a>
+                    <a href="">Github</a>
                     <img src="/icons/right-up.png" alt="" className="w-3 h-3" />
                 </div>
                 
               </div>
-              <a href="www.grouptikom.com" className="bg-black flex text-white py-4 px-3 rounded-lg mt-5 relative">
+              <a href="https://grouptikom.com/" className="bg-black flex text-white py-4 px-3 rounded-lg mt-5 relative">
                 Get IT Certification
-                <span className="bg-white absolute right-2 top-2 rounded-md py-2 px-2">
-                    <img src="/icons/right-up.png" alt="arrow" className="w-6 " />
+                <span className="bg-white absolute right-2.5 top-3 rounded-md py-2 px-2">
+                    <img src="/icons/right-up.png" alt="arrow" className="w-4" />
                 </span>
               </a>
             </motion.div>
